cc5724bfaaf5c035279b8a5d2f97893c62952882
Uzair Jawaid <uzair-jawaid@hotmail.com>
2019-06-02 01:23:42 -0400

    Count error occurrences and formatted into dataframe

--- readExcelExample.py
+++ readExcelExample.py
@@ -39,5 +39,40 @@ print(all_data["Heading 4"]) #Show only column 4, along with the data type and n
 errors = all_data["Heading 4"].count() #Get the number of rows in the last column
 print("Number of errors: ", errors)
 
+errorList = [] #list of unique errors
+
+#loop through rows
+for x in range(errors):
+        if all_data.iloc[x,3] not in errorList: #if the error isn't already defined in the list, add it to the list
+                errorList.append(all_data.iloc[x,3])
+                continue
+
+print("Job errors query: ", errorList)
+
+errorCount = [0]*len(errorList) #Empty list to hold the count of each error by index
+
+#loop through the errors column
+for y in range(errors):
+        #loop through the unique errors column
+        for z in range(len(errorList)):
+                #check for a match, and add 1 to the count each time there's a repetition of the error in the main list
+                if all_data.iloc[y,3] == errorList[z]:
+                        errorCount[z] += 1
+                else:
+                        continue
+
+print(errorCount)
+
+errorDB = [] #Create a series list to make a map of the error matching with its number of occurrences
+#loop through the errors list
+for a in range(len(errorList)):
+        #add a tuple for each error into the errorDB
+        errorDB.append((errorList[a], errorCount[a]))
+
+print(errorDB)
+errorDBLabels = ["Error", "Occurrences"] #labels for the new data frame
+errorData = pd.DataFrame.from_records(errorDB, columns = errorDBLabels) #data frame to display errors vs occurances
+print(errorData)
+
 #Show execution time for process
 print (time.process_time() - start_time, "seconds to execute") 
